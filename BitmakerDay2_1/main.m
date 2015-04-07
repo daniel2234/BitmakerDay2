8935fbc158b9b2e2011a263c389843d95f9270d9
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-07 10:30:18 -0400

    Initial Commit

---
+++ BitmakerDay2_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BitmakerDay2_1
+//
+//  Created by Daniel Kwiatkowski on 2015-04-07.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
